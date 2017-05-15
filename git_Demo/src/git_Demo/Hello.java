3199d0b116549852c6935e1203a3e2e4cd3867d4
addyxiao <onethousandth@163.com>
2017-05-15 16:53:44 +0800

    This is second time git_test!

---
+++ git_Demo/src/git_Demo/Hello.java
@@ -0,0 +1,10 @@
+package git_Demo;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,This is second time git test!");
+	}
+
+}
